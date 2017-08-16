a20bde65da65baba620093348f5134233b1418c7
LiJohnson <598420668@163.com>
2017-08-17 01:37:31 +0800

    build dist

--- dist/book/plugin.js
+++ dist/book/plugin.js
@@ -6,15 +6,28 @@ require([
 	$(document.body).on('click','.mermaid-container i.fa',function(){
 		$(this).parent().siblings('svg,pre').toggle();
 	});
+	var id = 0;
 	gitbook.events.bind('page.change', function () {
 		$('.lang-mermaid',gitbook.state.$book).each(function(item){
 			var $container = $('<div class=mermaid-container ></div>');
-			var svg = mermaidAPI.render(this.innerText);
+			var svg = mermaidAPI.render('mermaid-id-'+id++,this.innerText);
 			var href =  URL.createObjectURL(new Blob([ svg ],{type:'image/svg+xml'}))
 			$(this).parent().after($container);
 			$container.append(svg);
 			$container.append('<div class=mermaid-tool ><i class="fa fa-file-text-o" aria-hidden="true"></i> <a href="' + href + '" target=_blank class="fa fa-eye"></a></div>');
 			$container.append($(this).parent().hide());
-		}).removeClass('lang-mermaid')
+		});
+
+		$('a[href$=".mermaid"]',gitbook.state.$book).each(function(){
+			var $this = $(this);
+			$.get(this.href,(text)=>{
+				var $container = $('<div class=mermaid-container ></div>');
+				var svg = mermaidAPI.render('mermaid-id-'+id++,text);
+				var href =  URL.createObjectURL(new Blob([ svg ],{type:'image/svg+xml'}))
+				$this.after($container);
+				$container.append(svg);
+				$container.append('<div class=mermaid-tool ><i class="fa fa-file-text-o" aria-hidden="true"></i> <a href="' + href + '" target=_blank class="fa fa-eye"></a></div>');
+			});
+		});
 	});
 });
