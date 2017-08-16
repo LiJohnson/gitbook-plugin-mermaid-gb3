1d5d17ee1a296b4b214f6a679e9c066cfc8a13c8
LiJohnson <598420668@163.com>
2017-08-16 20:09:37 +0800

    1. render svg in frontend 2. swap svg and source text

--- book/plugin.js
+++ book/plugin.js
@@ -1,7 +1,18 @@
 require([
-  'gitbook'
-], function (gitbook) {
-  gitbook.events.bind('page.change', function () {
-    mermaid.init();
-  });
+  'gitbook',
+  'jquery'
+], function (gitbook,$) {
+	debugger
+	$(document.body).on('click','.mermaid-container .fa',function(){
+		$(this).siblings().toggle();
+	});
+	gitbook.events.bind('page.change', function () {
+		$('.lang-mermaid').each(function(item){
+			$(this).after(mermaidAPI.render(this.innerText)).hide();
+		})
+		.removeClass('lang-mermaid')
+		.parent()
+		.addClass('mermaid-container')
+		.append('<i class="fa file-text-o" aria-hidden="true"></i>');
+	});
 });
--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-var mermaidRegex = /^```mermaid((.*[\r\n]+)+?)?```$/im;
-
-function processMermaidBlockList(page) {
-
-  var match;
-
-  while ((match = mermaidRegex.exec(page.content))) {
-    var rawBlock = match[0];
-    var mermaidContent = match[1];
-    page.content = page.content.replace(rawBlock, '<div class="mermaid">' +
-      mermaidContent + '</div>');
-  }
-
-  return page;
-}
-
 module.exports = {
   website: {
     assets: './dist',
     css: [
-      'mermaid/mermaid.css'
+      'mermaid/mermaid.css',
+      'book/style.css'
     ],
     js: [
       'book/plugin.js'
     ]
-  },
-  hooks: {
-    'page:before': processMermaidBlockList
   }
 };
