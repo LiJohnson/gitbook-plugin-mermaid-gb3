8b72eda07d803f057d346c6069e939d3cf2790a8
LiJohnson <598420668@163.com>
2017-08-17 01:00:28 +0800

    build dist

--- dist/book/plugin.js
+++ dist/book/plugin.js
@@ -3,16 +3,18 @@ require([
   'jquery'
 ], function (gitbook,$) {
 	debugger
-	$(document.body).on('click','.mermaid-container .fa',function(){
-		$(this).siblings().toggle();
+	$(document.body).on('click','.mermaid-container i.fa',function(){
+		$(this).parent().siblings('svg,pre').toggle();
 	});
 	gitbook.events.bind('page.change', function () {
-		$('.lang-mermaid').each(function(item){
-			$(this).after(mermaidAPI.render(this.innerText)).hide();
-		})
-		.removeClass('lang-mermaid')
-		.parent()
-		.addClass('mermaid-container')
-		.append('<i class="fa fa-file-text-o" aria-hidden="true"></i>');
+		$('.lang-mermaid',gitbook.state.$book).each(function(item){
+			var $container = $('<div class=mermaid-container ></div>');
+			var svg = mermaidAPI.render(this.innerText);
+			var href =  URL.createObjectURL(new Blob([ svg ],{type:'image/svg+xml'}))
+			$(this).parent().after($container);
+			$container.append(svg);
+			$container.append('<div class=mermaid-tool ><i class="fa fa-file-text-o" aria-hidden="true"></i> <a href="' + href + '" target=_blank class="fa fa-eye"></a></div>');
+			$container.append($(this).parent().hide());
+		}).removeClass('lang-mermaid')
 	});
 });
